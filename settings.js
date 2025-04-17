eea1bbcae7b82f8a569744a2a1ae756c5d8092a1
imalsharandRx <155436482+imalsharandRx@users.noreply.github.com>
2025-04-17 12:52:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
+  author: 'IMALSHA',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: '𝖨𝖬𝖠𝖫𝖲𝖧𝖠 𝖱𝖠𝖭𝖣𝖨𝖯', // Your name
+  ownerNumber: '94711683227', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
